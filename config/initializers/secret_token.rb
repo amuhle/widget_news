e45739a68cc0316f3108476d8dd304bbfce8e036
Pablo Vilas <pablo@mac.local>
2013-09-02 13:40:09 -0300

    Created widget_news project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WidgetNews::Application.config.secret_key_base = 'b6209600de82027de379e3240ed9e4a00949ace5a49e58eb18afe2d5c469ac6e567e84d389e1463e1a6532acb889696a37e86a29e22a81bfe075513ca237fc05'
