88aa4101bf43fd5a03be3536faec80bfb7172718
Pablo Vilas <pablo@mac.local>
2013-09-03 12:02:20 -0300

    Added Gemfile and routes to git

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 WidgetNews::Application.routes.draw do
+
+  resource 'widget_loaders'
+
+  get 'load_widget'  => 'widget_loaders#load_widget'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
