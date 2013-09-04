726f7d36fe7386f0ef84ed3b1699ade754494776
Pablo Vilas <pablo@mac.local>
2013-09-04 15:35:22 -0300

    Copied widget html for future customizations

--- app/controllers/widget_loaders_controller.rb
+++ app/controllers/widget_loaders_controller.rb
@@ -2,7 +2,7 @@ class WidgetLoadersController < ApplicationController
 
   def load_widget
 
-    file = File.read(File.join('spec', 'fixtures', 'index.html'))
+    file = File.read(File.join('app', 'views', 'widget_loaders', 'index.html'))
     html = file || '<h1>No hay HTML</h1>'
     a = render js: params[:callback] + "(#{{ html: html }.to_json})"
     puts a
