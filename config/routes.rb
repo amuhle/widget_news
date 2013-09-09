93e1698dae8bf2a9b7546a25a8d0e5f82ad72afe
Pablo Vilas <pablo@mac.local>
2013-09-09 13:02:37 -0300

    Deleted widget test files, added sccafold for widget designer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 WidgetNews::Application.routes.draw do
 
   resource 'widgets'
+  resource 'designers'
 
   get 'load_widget'  => 'widgets#load_widget'
+  
+  root 'designers#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
